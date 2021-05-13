85e85e11e89668a899fd0bb6ab36fb39cd80fabc
IronRoma999 <70891317+IronRoma999@users.noreply.github.com>
2021-05-13 10:58:23 +0300

    new

--- pemark.js
+++ pemark.js
@@ -8,8 +8,31 @@ const model = [
     ]}
 ]
 
-const $site = document.querySelector (selectors: '#site')
+const $site = document.querySelector('#site')
 
 model.forEach(block => {
-    console.log(block)
+    let html = ``
+
+    if (block.type === 'title') {
+        html = `
+            <div class="row">
+                <div class="col-sm">
+                    <h1>${block.value}</h1>
+                </div>
+            </div>
+        `
+    } else if (block.type == 'text') {
+        html = `
+            <div class="row">
+                <div class="col-sm">
+                    <p>${block.value}</p>
+            </div>
+        </div>
+        `
+    } else if (block.type == 'columns') {
+    
+    }
+
+    $site.insertAdjacentHTML('beforeend', html)
+
 })
